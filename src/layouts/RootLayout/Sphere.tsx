6532d36a61affe4032180d1a4852490110a58143
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2025-09-16 21:06:46 -0300

    style: Add Stack View responsiveness

--- src/layouts/RootLayout/Sphere.tsx
+++ src/layouts/RootLayout/Sphere.tsx
@@ -8,7 +8,7 @@ interface SphereProps {
 
 const Sphere: React.FC<SphereProps> = () => {
   return (
-    <div className="relative sphere-container w-full">
+    <div className="sphere-container w-full">
       <div className="sphere flex items-start justify-center h-screen bg-black">
         <Button className="mt-20 max-md:text-xs 2xl:text-lg">
           <Icon src="/icons/download-icon.svg" alt="download" />
