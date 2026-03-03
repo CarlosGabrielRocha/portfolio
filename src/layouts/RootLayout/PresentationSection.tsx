eb697cdb0de4904564889d9b5955e7948555b68d
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2026-03-03 18:43:04 -0300

    fix: Fix on main title

--- src/layouts/RootLayout/PresentationSection.tsx
+++ src/layouts/RootLayout/PresentationSection.tsx
@@ -11,7 +11,7 @@ const PresentationSection: React.FC = () => {
       className="w-full pt-15 2xl:pt-20 bg-gradient-to-b from-darker-blue via-50% to-[#0B2846] overflow-x-hidden"
       id="presentation"
     >
-      <SilverTitle className="text-center font-semibold lg:leading-16 2xl:leading-20 px-2 mx-auto my-30 2xl:my-48 max-w-120 lg:max-w-140 2xl:max-w-170">
+      <SilverTitle className="text-center font-semibold lg:leading-14 2xl:leading-18 px-2 mx-auto my-30 2xl:my-48 max-w-120 lg:max-w-140 2xl:max-w-170">
         {t("presentation.title")}
       </SilverTitle>
       <Satellite />
