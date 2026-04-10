2f4312ebce7405511e604df886e3876f14341ae3
Carlos Gabriel <carlosgabrielweb2003@gmail.com>
2026-04-10 18:28:57 -0300

    add traduction to curriculo

--- src/layouts/RootLayout/Sphere.tsx
+++ src/layouts/RootLayout/Sphere.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import Button from "../../components/Button";
 import Icon from "../../components/icons/Icon";
+import i18n from "../../i18n";
 
 interface SphereProps {
   children?: ReactNode;
@@ -10,7 +11,7 @@ const Sphere: React.FC<SphereProps> = () => {
   return (
     <div className="sphere-container w-full">
       <div className="sphere flex items-start justify-center h-screen bg-black">
-        <Button className="mt-20 max-md:text-xs 2xl:text-lg" href="/curriculo-carlos-gabriel.pdf" download={true}>
+        <Button className="mt-20 max-md:text-xs 2xl:text-lg" href={i18n.language === "en" ? "/carlos-curriculo-english.pdf" : "/carlos-curriculo-portuguese.pdf"} download={true}>
           <Icon src="/icons/download-icon.svg" alt="download" />
           Download CV
         </Button>
